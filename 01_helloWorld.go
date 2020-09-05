139c4f133b2bfe7b7f556a59f16477bc8ace1c80
ashish22arya <ashish22arya@gmail.com>
2020-09-05 14:52:14 +0530

    Added golang variables and constants tutorial.

---
+++ 01_helloWorld.go
@@ -0,0 +1,9 @@
+// Simplest Go program
+
+package main		// Defining the main program
+
+import "fmt"		// Importing standard library package to print to console.
+
+func main() {		// Where the execution of program begins.
+	fmt.Println("Hello World.")
+}
